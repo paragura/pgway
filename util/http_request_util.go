b15645de70b0e5b1010aa83c4f0d1cfdc469027c
paragura <paragura5041@gmail.com>
2018-09-18 12:35:46 +0900

    [speed up]url sanitize(only forward slash) without regex

--- util/http_request_util.go
+++ util/http_request_util.go
@@ -1,8 +1,19 @@
 package util
 
-import "regexp"
-
-func UrlSanitize(urlStr string) string {
-	reg := regexp.MustCompile(`/+`)
-	return reg.ReplaceAllString(urlStr, "/")
+func UrlSanitize(pathStr string) string {
+	var retRunes []rune
+	var isSlash = false
+	for _, pathRune := range pathStr {
+		if pathRune == '/' {
+			if isSlash {
+				continue
+			} else {
+				isSlash = true
+			}
+		} else {
+			isSlash = false
+		}
+		retRunes = append(retRunes, pathRune)
+	}
+	return string(retRunes)
 }
