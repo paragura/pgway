38963d809b0921095d17e9f15cbea272140eb6fe
muro_wataru <muro_wataru@nintendo.co.jp>
2018-09-20 14:09:47 +0900

    [implementation][routing with tree algorithm]fix

--- api/pgway_router.go
+++ api/pgway_router.go
@@ -1,63 +1,74 @@
 package api
 
-import (
-	"pgway/model"
-)
-
 type PgwayRoute struct {
 }
 
 type PgwayRouteTree struct {
-	Nodes       map[string]PgwayRouteNode
+	Nodes       map[string]*PgwayRouteNode
 	initialized bool
 }
 
 type PgwayRouteNode struct {
-	Path                string                    // pathの一部
-	IsPathVariable      bool                      //
-	PathVariableKeyName string                    //
-	Nodes               map[string]PgwayRouteNode //
-	IsEndNode           bool                      // is end (it means the routing exists.)
-	Handler             interface{}               //
-	Exists              bool
+	Path                string                     // pathの一部
+	IsPathVariable      bool                       //
+	PathVariableKeyName string                     //
+	Nodes               map[string]*PgwayRouteNode //
+	IsEndNode           bool                       // is end (it means the routing exists.)
+	Api                 PgwayApi                   //
 }
 
 const PathVariableNodePath = "/" // because / is not use for url node.
 
-func createTree(server *PgwayServer) PgwayRouteTree {
-	tree := PgwayRouteTree{}
+func CreateTree(server *PgwayServer) PgwayRouteTree {
+	tree := PgwayRouteTree{Nodes: map[string]*PgwayRouteNode{}}
 	for _, api := range server.Apis {
 		tree.addRoute(&api)
 	}
 	return tree
 }
 
-func (tree *PgwayRouteTree) tracePath(request *PgwayRequest) (interface{}, map[string]string) {
+func ShowNodes(nodes map[string]*PgwayRouteNode) {
+	if len(nodes) == 0 {
+		return
+	}
+
+	for key, node := range nodes {
+		println("key:" + key + "->" + node.Api.Path)
+		ShowNodes(node.Nodes)
+	}
+}
+
+func (tree *PgwayRouteTree) tracePath(request *PgwayRequest) (*PgwayApi, map[string]string) {
 
 	pathRunes := []rune(request.Path)
 	node := tree.Nodes[request.HTTPMethod]
 	pathVariables := map[string]string{}
-	targetNode := node.findNext(pathRunes, 0, len(request.Path), pathVariables)
-	if targetNode.IsEndNode {
-		return targetNode, pathVariables
+	targetNode := findNext(node, pathRunes, 0, len(request.Path), pathVariables)
+	if targetNode != nil && targetNode.IsEndNode {
+		return &targetNode.Api, pathVariables
 	} else {
 		//
 		// request path is added but is not end node.
-		return model.ApiNotFound, pathVariables
+		return nil, pathVariables
 	}
 }
 
-func (node *PgwayRouteNode) findNext(pathRunes []rune, i int, pathLen int, pathVariables map[string]string) PgwayRouteNode {
-	if !node.Exists || i == pathLen-1 || (i == pathLen-2 && pathRunes[i] == '/') {
+func findNext(node *PgwayRouteNode, pathRunes []rune, i int, pathLen int, pathVariables map[string]string) *PgwayRouteNode {
+	if node == nil {
+		return nil
+	}
+
+	if i == pathLen || (i == pathLen-1 && pathRunes[i] == '/') {
 		// finish
-		return *node
+		return node
 	}
 
 	if pathRunes[i] != '/' {
 		//
 		// request path format is invalid.
-		return PgwayRouteNode{} //model.ApiNotFound,pathVariables
+		return nil
 	}
+	i++
 
 	var pathPart []rune
 	for ; i < pathLen && pathRunes[i] != '/'; i++ {
@@ -65,18 +76,32 @@ func (node *PgwayRouteNode) findNext(pathRunes []rune, i int, pathLen int, pathV
 	}
 	pathPartStr := string(pathPart)
 	currentNode := node.Nodes[pathPartStr]
-	currentNode.findNext(pathRunes, i, pathLen, pathVariables)
-	if currentNode.Exists {
+	currentNode = findNext(currentNode, pathRunes, i, pathLen, pathVariables)
+	if currentNode != nil {
 		return currentNode
 	}
 	//
 	// check path(variable type).
 	currentNode = node.Nodes[PathVariableNodePath]
-	if currentNode.Exists {
+	if currentNode != nil {
 		pathVariables[currentNode.PathVariableKeyName] = pathPartStr
-		return currentNode.findNext(pathRunes, i, pathLen, pathVariables)
+		return findNext(currentNode, pathRunes, i, pathLen, pathVariables)
 	} else {
-		return PgwayRouteNode{}
+		return nil
+	}
+}
+
+func getOrCreateNode(key string, nodes map[string]*PgwayRouteNode) *PgwayRouteNode {
+	if node, ok := nodes[key]; ok {
+		return node
+	} else {
+		newNode := &PgwayRouteNode{
+			IsPathVariable: false,
+			IsEndNode:      false,
+			Nodes:          map[string]*PgwayRouteNode{},
+		}
+		nodes[key] = newNode
+		return newNode
 	}
 }
 
@@ -84,14 +109,11 @@ func (tree *PgwayRouteTree) addRoute(api *PgwayApi) {
 	pathRunes := []rune(api.Path)
 	pathLen := len(api.Path)
 
-	currentNode := tree.Nodes[api.HTTPMethod]
-	//
-	// config baseNode
-	currentNode.Exists = true
+	currentNode := getOrCreateNode(api.HTTPMethod, tree.Nodes)
 
-	for i := 0; i < pathLen; i++ {
+	for i := 0; i < pathLen; {
 		if pathRunes[i] != '/' {
-			panic("invalid path given.")
+			panic("[PGWay]{Router]invalid path given.")
 		}
 		i++
 		var pathPart []rune
@@ -101,12 +123,12 @@ func (tree *PgwayRouteTree) addRoute(api *PgwayApi) {
 
 		if pathPart[0] == ':' {
 			// path variable
-			currentNode = currentNode.Nodes[PathVariableNodePath]
+			currentNode = getOrCreateNode(PathVariableNodePath, currentNode.Nodes)
 			currentNode.Path = PathVariableNodePath
 			pathVariableKeyName := string(pathPart[1:])
 			if currentNode.IsPathVariable {
 				if currentNode.PathVariableKeyName != pathVariableKeyName {
-					panic("[PGWay][route]can't add route: pathvariable name has changed. " + currentNode.PathVariableKeyName + "->" + pathVariableKeyName)
+					panic("[PGWay][Router]can't add route: path variable name has changed. " + currentNode.PathVariableKeyName + "->" + pathVariableKeyName)
 				}
 			} else {
 				currentNode.PathVariableKeyName = pathVariableKeyName
@@ -114,12 +136,14 @@ func (tree *PgwayRouteTree) addRoute(api *PgwayApi) {
 			}
 		} else {
 			key := string(pathPart)
-			currentNode = currentNode.Nodes[key]
+			currentNode = getOrCreateNode(key, currentNode.Nodes)
 			currentNode.Path = key
 		}
-		currentNode.Exists = true
 	}
-	currentNode.Handler = api.Handler
-	currentNode.IsEndNode = true
-
+	if currentNode.IsEndNode {
+		panic("[PGWay][Router] duplicate path")
+	} else {
+		currentNode.Api = *api
+		currentNode.IsEndNode = true
+	}
 }
