4f4eff70afe6c0e313fddd857f5828ddcbfc6587
paragura <paragura5041@gmail.com>
2018-09-26 13:29:04 +0900

    [issue][feature]remove deprecated funcs

--- pgway_api_test.go
+++ pgway_api_test.go
@@ -1,15 +1,1 @@
 package pgway
-
-import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
-
-func TestPgwayApi_IsSamePath(t *testing.T) {
-	api := Api{
-		Path: "fe",
-	}
-
-	assert.Equal(t, true, api.IsSamePath("fe"))
-
-}
